8dac1dc6d87aa345623dab14043ea05c13b1224b
oerlex <alex.sp@posteo.de>
2015-10-05 12:34:03 +0200

    why ?

--- YachtClub/YachtClub/Model/Boat.cs
+++ YachtClub/YachtClub/Model/Boat.cs
@@ -2,35 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace YachtClub
 {
-    public class Boat : FileInteraction
-    {
-
-
-     
-
+    public class Boat : FileInteraction {
 
         private double length;
-        
-
-        public double Length { get { return length; }  set { length = value;  }  }
+        private string type;
 
 
-        public enum boat_Type { sailboat, motorsailer, kayak, other };
-
-        private boat_Type _Difficulty;
-
-    public void SetType(boat_Type difficulty)
-    {
-    _Difficulty = difficulty;
-    }
-
-    public boat_Type GetType()
-    {
-    return _Difficulty;
-    }   
-
+        public double Length { get { return length; } set{ length = value; } }
+        public string Type { get { return type; } set { type = value; } }
     }
 }
--- YachtClub/YachtClub/Model/Member.cs
+++ YachtClub/YachtClub/Model/Member.cs
@@ -2,25 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
-namespace YachtClub
-{
-    public class Member : FileInteraction
-    {
+namespace YachtClub{
+    public class Member : FileInteraction{
         
         private string last_Name;
         private string first_Name;
         private int personal_Number;
-        private in member_Id;
+        private int member_Id;
         private List<Boat> boats;
 
         public string last_name { get { return last_Name; } set { value = last_Name; } }
         public string first_name { get { return first_Name; } set { value = first_Name; } }
         public int personal_number { get { return personal_Number; } set { value = personal_Number; } }
         public int member_id { get {return member_Id; } set{ value = member_Id;}}
-        public List<Boat> boaths { get{ return boats; set{ value = boaths; }
+        public List<Boat> boaths { get { return boats; } set{ value = boats; } }
+
+
+
+        /// <summary>
+        /// Adds a new member and all his boats to the xml file (like a saving process)
+        /// </summary>
+        public override void add(){
+            XmlDocument doc = new XmlDocument();
+            doc.Load("Storage.xml");
+            XmlElement id = doc.CreateElement(myGuid.ToString());
+            id.SetAttribute(last_Name, first_Name, personal_Number.ToString());
+
+            XmlElement xBoats = doc.CreateElement("boats");
+            id.AppendChild(xBoats);
+
+            foreach (Boat b in boats)
+            {
+                Boat newBoat = new Boat();
+                XmlElement xNewBoat = doc.CreateElement(newBoat.myGuid.ToString());
+                xNewBoat.SetAttribute(newBoat.Type, newBoat.Length.ToString());
+                xBoats.AppendChild(xNewBoat);
+            }
+
+            doc.AppendChild(id);
+
+
+        }
     }
+}
 
    
-}
+
 
