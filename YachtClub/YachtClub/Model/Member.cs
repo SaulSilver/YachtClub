d280a4b9e26e7b380eefa097a8b2ab0e0d757e60
oerlex <alex.sp@posteo.de>
2015-10-05 13:32:00 +0200

    add member and add boat should work now. have to test it tomorrow

--- YachtClub/YachtClub/Model/Member.cs
+++ YachtClub/YachtClub/Model/Member.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace YachtClub
 {
     public class Member : FileInteraction
-    {
-        
+    {        
         private string last_Name;
         private string first_Name;
         private int personal_Number;
@@ -18,7 +18,29 @@ namespace YachtClub
         public string first_name { get { return first_Name; } set { value = first_Name; } }
         public int personal_number { get { return personal_Number; } set { value = personal_Number; } }
         public int member_id { get {return member_Id; } set{ value = member_Id;}}
-        public List<Boat> boats { get{ return boats; } set{ value = boats; }
+        public List<Boat> boats { get { return boats; } set { value = boats; } }
+
+        /// <summary>
+        /// Adds a new member and all his boats to the xml file (like a saving process)
+        /// </summary>
+        public override void add(){
+            XmlDocument doc = new XmlDocument();
+            doc.Load("Storage.xml");
+            XmlElement id = doc.CreateElement(myGuid.ToString());
+            id.SetAttribute(last_Name, first_Name, personal_Number.ToString());
+
+            XmlElement xBoats = doc.CreateElement("boats");
+            id.AppendChild(xBoats);
+
+            foreach (Boat b in boats){
+                Boat newBoat = new Boat();
+                XmlElement xNewBoat = doc.CreateElement(newBoat.myGuid.ToString());
+                xNewBoat.SetAttribute(newBoat.GetType().ToString(), newBoat.Length.ToString());
+                xBoats.AppendChild(xNewBoat);
+            }
+            doc.AppendChild(id);
+        }      
+        
     }
 
    
