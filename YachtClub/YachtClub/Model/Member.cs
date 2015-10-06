2b88047245696d0ea275df2b2ad38554679c6638
SaulSilver <hatem.houssein@gmail.com>
2015-10-06 10:15:52 +0200

    Controller test class

--- YachtClub/YachtClub/Model/Member.cs
+++ YachtClub/YachtClub/Model/Member.cs
@@ -7,7 +7,7 @@ namespace YachtClub
 {
     public class Member : FileInteraction
     {
-        
+
         private string last_Name;
         private string first_Name;
         private int personal_Number;
@@ -17,10 +17,13 @@ namespace YachtClub
         public string last_name { get { return last_Name; } set { value = last_Name; } }
         public string first_name { get { return first_Name; } set { value = first_Name; } }
         public int personal_number { get { return personal_Number; } set { value = personal_Number; } }
-        public int member_id { get {return member_Id; } set{ value = member_Id;}}
-        public List<Boat> boats { get{ return boats; } set{ value = boats; }
-    }
+        public int member_id { get { return member_Id; } set { value = member_Id; } }
+        public List<Boat> boats
+        {
+            get { return boats; }
+            set { value = boats; }
+        }
 
-   
+    }
 }
 
